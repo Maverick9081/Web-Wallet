b0ad196513f1dc6ce92dac2afe68cb5fcbde20a7
Maverick9081 <faliyaabhishek@gmail.com>
2022-04-14 00:28:28 +0530

    Refactored a bearer code

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -8,17 +8,17 @@ export const verifyToken = async (req,res,next) => {
     const bearerHeader = req.headers['authorization'];
 
     if(typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
+        const bearer = bearerHeader.split(' ')[1];
 
-        const bearerToken = bearer[1];
         try{
-        const user = jwt.verify(bearerToken, process.env.JWT_PRIVATE_KEY);
-            if(!user){
-                res.status(403)
-                    .send('Yor are not logged in or User timeOut');
-                return;
-            }
-        }catch(error) {
+            const user = jwt.verify(bearer, process.env.JWT_PRIVATE_KEY);
+                if(!user){
+                    res.status(403)
+                        .send('Yor are not logged in or User timeOut');
+                    return;
+                }
+        }
+        catch(error){
             console.log(error);
         }
         next();
